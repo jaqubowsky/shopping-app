f7929bf6f4677f6345a8820903a3dccb89c27ded
Jakub Nalewajk <jaqubowskyy@gmail.com>
2023-04-06 14:06:07 +0200

    Add navigation

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -5,24 +5,21 @@ import {
   StyledLink,
   StyledNav,
   StyledLinksWrapper,
-  StyledLogo,
-  StyledCartIcon,
-  StyledIconWrapper
+  StyledLogo
 } from "./Layout.styled";
+import CartButton from "../Cart/CartButton.jsx";
 
 function Layout() {
   return (
     <>
       <StyledHeader>
         <StyledNav>
-          <StyledLogo>FakeStore</StyledLogo>
+          <StyledLogo to=".">FakeStore</StyledLogo>
           <StyledLinksWrapper>
-            <StyledLink>Home</StyledLink>
-            <StyledLink>Products</StyledLink>
-            <StyledLink>Contact</StyledLink>
-            <StyledIconWrapper>
-              <StyledCartIcon />
-            </StyledIconWrapper>
+            <StyledLink to="/">Home</StyledLink>
+            <StyledLink to="products">Products</StyledLink>
+            <StyledLink to="contact">Contact</StyledLink>
+            <CartButton />
           </StyledLinksWrapper>
         </StyledNav>
       </StyledHeader>

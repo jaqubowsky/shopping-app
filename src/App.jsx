2eaedced29a058eff0db9989a7176212a92b4ee4
Jakub Nalewajk <jaqubowskyy@gmail.com>
2023-04-06 14:06:15 +0200

    Fix routing

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,16 @@ import {
 } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
 import Home from "./pages/Home/Home";
+import Products from "./pages/Products/Products";
+import Contact from "./pages/Contact/Contact";
 
 function App() {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/" element={<Layout />}>
-        <Route path="home" element={<Home />} />
+        <Route index element={<Home />} />
+        <Route index path="products" element={<Products />} />
+        <Route index path="contact" element={<Contact />} />
       </Route>
     )
   );

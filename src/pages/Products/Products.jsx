779e8149abd9e213a514c32281e874f949921741
Jakub Nalewajk <jaqubowskyy@gmail.com>
2023-04-07 17:20:46 +0200

    Add sample products component

--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -1,3 +1,26 @@
+import { defer, useLoaderData, Await } from "react-router-dom";
+import { Suspense } from "react";
+import getProducts from "../../api";
+
+export async function loader() {
+  return defer({ products: getProducts() });
+}
+
 export default function Products() {
-  return <h1>Sample products page</h1>;
+  const productsData = useLoaderData();
+
+  return (
+    <Suspense fallback={<h1>Loading...</h1>}>
+      <Await resolve={productsData.products}>
+        {(products) =>
+          products.map((product) => (
+            <div>
+              <h1>{product.title}</h1>
+              <img src={product.image} />
+            </div>
+          ))
+        }
+      </Await>
+    </Suspense>
+  );
 }

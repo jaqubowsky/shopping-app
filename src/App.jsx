7131a9c2f31ebcd66ce0121c3c55db0716117cca
Jakub Nalewajk <jaqubowskyy@gmail.com>
2023-04-09 01:25:49 +0200

    Add Cart Context functionality

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,27 @@ import {
   Route,
   RouterProvider
 } from "react-router-dom";
+import { useState, useMemo } from "react";
 import Layout from "./components/Layout/Layout";
 import Home from "./pages/Home/Home";
+import CartContext from "./components/Cart/CartContext";
 import Products, { loader as productsLoader } from "./pages/Products/Products";
 import Contact from "./pages/Contact/Contact";
 
 function App() {
+  const [cartItems, setCartItems] = useState([]);
+  const updateCartItems = (item) => {
+    setCartItems((prevItems) => [...prevItems, item]);
+  };
+
+  const providerValue = useMemo(
+    () => ({
+      cartItems,
+      updateCartItems
+    }),
+    [cartItems, setCartItems, updateCartItems]
+  );
+
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/" element={<Layout />}>
@@ -20,7 +35,13 @@ function App() {
     )
   );
 
-  return <RouterProvider router={router} />;
+  console.log(cartItems);
+
+  return (
+    <CartContext.Provider value={providerValue}>
+      <RouterProvider router={router} />
+    </CartContext.Provider>
+  );
 }
 
 export default App;

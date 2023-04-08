cd2d15b3d061a2e6df3b90d7ad2cf2d0522f2162
Jakub Nalewajk <jaqubowskyy@gmail.com>
2023-04-09 01:25:57 +0200

    Fix styling

--- src/components/Cart/Cart.styled.jsx
+++ src/components/Cart/Cart.styled.jsx
@@ -5,14 +5,21 @@ export const StyledCart = styled(motion.div)`
   position: absolute;
   right: 0;
   background: white;
-  width: 20%;
+  width: 700px;
+  max-width: 30%;
   padding: 2em;
   height: 100vh;
   z-index: 9999;
   text-align: center;
 `;
 
-export const ButtonWrapper = styled.div`
+export const StyledCartElementsWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 3em;
+`;
+
+export const StyledButtonWrapper = styled.div`
   display: flex;
   flex-direction: column;
 `;

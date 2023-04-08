c90b8c6bb28d993c049ee01035bcf25adf6e9494
Jakub Nalewajk <jaqubowskyy@gmail.com>
2023-04-09 01:26:24 +0200

    Add styling to products wrapper

---
+++ src/pages/Products/Products.styled.jsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const StyledProductsWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  margin: 0 auto;
+  width: 80%;
+  gap: 2em;
+`;
+
+export default StyledProductsWrapper;

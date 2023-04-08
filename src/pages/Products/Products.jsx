4b0ba1019e5d29c37b20f4e66dc167545d48c68c
Jakub Nalewajk <jaqubowskyy@gmail.com>
2023-04-09 01:26:14 +0200

    Clean code

--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -1,26 +1,39 @@
 import { defer, useLoaderData, Await } from "react-router-dom";
-import { Suspense } from "react";
+import { Suspense, useContext } from "react";
+import StyledProductsWrapper from "./Products.styled";
 import getProducts from "../../api";
+import ProductCard from "./ProductCard";
+import CartContext from "../../components/Cart/CartContext";
 
 export async function loader() {
-  return defer({ products: getProducts() });
+  return defer({
+    products: getProducts()
+  });
 }
 
 export default function Products() {
   const productsData = useLoaderData();
 
+  const { updateCartItems } = useContext(CartContext);
+
+  function renderProducts(products) {
+    return products.map((product) => (
+      <ProductCard
+        item={product}
+        key={product.id}
+        name={product.title}
+        price={product.price}
+        img={product.image}
+        handleUpdateCart={() => updateCartItems(product)}
+      />
+    ));
+  }
+
   return (
     <Suspense fallback={<h1>Loading...</h1>}>
-      <Await resolve={productsData.products}>
-        {(products) =>
-          products.map((product) => (
-            <div>
-              <h1>{product.title}</h1>
-              <img src={product.image} />
-            </div>
-          ))
-        }
-      </Await>
+      <StyledProductsWrapper>
+        <Await resolve={productsData.products}>{renderProducts}</Await>
+      </StyledProductsWrapper>
     </Suspense>
   );
 }

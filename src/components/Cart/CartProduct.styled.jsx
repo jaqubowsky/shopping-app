ac3c28d091b5dd1d8fcfff6a2360865e804aa86d
Jakub Nalewajk <jaqubowskyy@gmail.com>
2023-04-09 01:25:02 +0200

    Add styling to card products components

---
+++ src/components/Cart/CartProduct.styled.jsx
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+
+export const StyledCartProductWrapper = styled.div`
+  display: flex;
+  gap: 3em;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-content: center;
+  width: 90%;
+  margin: 0 auto;
+`;
+
+export const StyledDescriptionWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-content: center;
+`;
+
+export const StyledImage = styled.img`
+  width: 100px;
+  height: 100%;
+`;
+
+export const StyledQuantityWrapper = styled.div`
+  display: flex;
+  gap: 1em;
+`;
+
+export const StyledAddQuantityButton = styled.button``;
+
+export const StyledDecreaseQuantityButton = styled.button``;
---
+++ src/pages/Products/ProductCard.styled.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const StyledProductCardWrapper = styled.div`
+  display: flex;
+  width: 30%;
+  flex-direction: column;
+  justify-content: flex-end;
+  align-content: center;
+`;
+export const StyledProductImage = styled.img`
+  margin: 0 auto;
+  height: 350px;
+  padding: 5rem;
+`;

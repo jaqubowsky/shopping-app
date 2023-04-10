3cc2dff5f88f3ebb3ceba71dfff4df96be2dd9d1
Jakub Nalewajk <jaqubowskyy@gmail.com>
2023-04-11 01:39:41 +0200

    Add styling to ProductCard

--- src/pages/Products/ProductCard.styled.jsx
+++ src/pages/Products/ProductCard.styled.jsx
@@ -2,13 +2,42 @@ import styled from "styled-components";
 
 export const StyledProductCardWrapper = styled.div`
   display: flex;
-  width: 30%;
   flex-direction: column;
-  justify-content: flex-end;
-  align-content: center;
+  border: 1px solid rgb(229, 229, 229);
+  font-size: 0.8rem;
+  text-align: left;
 `;
+
+export const StyledPrice = styled.span`
+  font-size: 1.3rem;
+  color: #222222;
+`;
+
+export const StyledUtils = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 1.2em;
+  gap: 1.2em;
+  justify-content: space-between;
+  height: 45%;
+`;
+
+export const StyledDescription = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  gap: 1rem;
+`;
+
 export const StyledProductImage = styled.img`
+  height: 100%;
   margin: 0 auto;
-  height: 350px;
-  padding: 5rem;
+`;
+
+export const StyledProductImageContainer = styled.div`
+  height: 25em;
+  padding: 3rem;
+  width: 100%;
+  margin: 0 auto;
+  border-bottom: 1px solid rgb(229, 229, 229);
 `;

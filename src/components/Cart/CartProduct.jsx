07613c6827f9a1fef9626e8a6fd17aa0c04f6ca7
Jakub Nalewajk <jaqubowskyy@gmail.com>
2023-04-11 01:41:20 +0200

    Add styling to CartProduct

--- src/components/Cart/CartProduct.jsx
+++ src/components/Cart/CartProduct.jsx
@@ -3,20 +3,25 @@ import {
   StyledCartProductWrapper,
   StyledDescriptionWrapper,
   StyledQuantityWrapper,
-  StyledAddQuantityButton,
-  StyledDecreaseQuantityButton,
-  StyledImage
+  StyledImage,
+  StyledQuantity,
+  StyledPrice
 } from "./CartProduct.styled";
+import Button from "../Button/Button";
 import truncate from "../../utils";
 
-function CartProduct({ img, title, price, quantity }) {
+function CartProduct({ img, title, price, quantity, handleAddQuantity, handleDecreaseQuantity }) {
   return (
     <StyledCartProductWrapper>
       <StyledImage src={img} alt={title} />
       <StyledDescriptionWrapper>
-        <h4>{truncate(title, 15)}</h4>
-        <span>{`$${price}`}</span>
-        <StyledQuantityWrapper>{quantity}</StyledQuantityWrapper>
+        <h3>{truncate(title, 15)}</h3>
+        <StyledPrice>{`$${(price * quantity).toFixed(2)}`}</StyledPrice>
+        <StyledQuantityWrapper>
+          <Button name="-" handleClick={handleDecreaseQuantity} color="grey" />
+          <StyledQuantity>{quantity}</StyledQuantity>
+          <Button name="+" handleClick={handleAddQuantity} color="grey" />
+        </StyledQuantityWrapper>
       </StyledDescriptionWrapper>
     </StyledCartProductWrapper>
   );
@@ -26,7 +31,9 @@ CartProduct.propTypes = {
   img: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   price: PropTypes.number.isRequired,
-  quantity: PropTypes.number
+  quantity: PropTypes.number,
+  handleAddQuantity: PropTypes.func.isRequired,
+  handleDecreaseQuantity: PropTypes.func.isRequired
 };
 
 CartProduct.defaultProps = {
--- src/components/Cart/CartProduct.styled.jsx
+++ src/components/Cart/CartProduct.styled.jsx
@@ -7,7 +7,7 @@ export const StyledCartProductWrapper = styled.div`
   justify-content: flex-start;
   align-content: center;
   width: 90%;
-  margin: 0 auto;
+  margin: 0 auto 3em auto;
 `;
 
 export const StyledDescriptionWrapper = styled.div`
@@ -15,6 +15,7 @@ export const StyledDescriptionWrapper = styled.div`
   flex-direction: column;
   justify-content: center;
   align-content: center;
+  gap: 0.5em;
 `;
 
 export const StyledImage = styled.img`
@@ -22,11 +23,18 @@ export const StyledImage = styled.img`
   height: 100%;
 `;
 
+export const StyledQuantity = styled.span`
+  font-size: 1.1rem;
+  font-weight: bolder;
+  color: #222222;
+`;
+
+export const StyledPrice = styled.h4`
+  color: #222222;
+`;
+
 export const StyledQuantityWrapper = styled.div`
   display: flex;
   gap: 1em;
+  align-items: center;
 `;
-
-export const StyledAddQuantityButton = styled.button``;
-
-export const StyledDecreaseQuantityButton = styled.button``;

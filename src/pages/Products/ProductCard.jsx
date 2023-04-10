c1cb94b0fb4521375839d9e9d6c01efb8069c5cf
Jakub Nalewajk <jaqubowskyy@gmail.com>
2023-04-11 01:39:52 +0200

    Add ProductCart component

--- src/pages/Products/ProductCard.jsx
+++ src/pages/Products/ProductCard.jsx
@@ -1,14 +1,27 @@
 import PropTypes from "prop-types";
-import { StyledProductCardWrapper, StyledProductImage } from "./ProductCard.styled";
+import {
+  StyledProductCardWrapper,
+  StyledProductImage,
+  StyledPrice,
+  StyledUtils,
+  StyledDescription,
+  StyledProductImageContainer
+} from "./ProductCard.styled";
 import Button from "../../components/Button/Button";
 
 export default function ProductCard({ name, price, img, handleUpdateCart }) {
   return (
     <StyledProductCardWrapper>
-      <StyledProductImage src={img} />
-      <h2>{name}</h2>
-      <span>${price}</span>
-      <Button name="Add to cart" color="black" handleClick={handleUpdateCart} />
+      <StyledProductImageContainer>
+        <StyledProductImage src={img} />
+      </StyledProductImageContainer>
+      <StyledUtils>
+        <StyledDescription>
+          <h2>{name}</h2>
+          <StyledPrice>${price}</StyledPrice>
+        </StyledDescription>
+        <Button name="Add to cart" color="black" handleClick={handleUpdateCart} />
+      </StyledUtils>
     </StyledProductCardWrapper>
   );
 }

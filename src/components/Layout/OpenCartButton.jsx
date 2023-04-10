ba998cb3417749a1c8d625f655504d8882b6bfcc
Jakub Nalewajk <jaqubowskyy@gmail.com>
2023-04-11 01:42:35 +0200

    Add quantity visibility on cart button

--- src/components/Layout/OpenCartButton.jsx
+++ src/components/Layout/OpenCartButton.jsx
@@ -1,10 +1,14 @@
 import PropTypes from "prop-types";
-import { StyledCartButton, StyledCartIcon } from "./OpenCartButton.styled.jsx";
+import { StyledCartButton, StyledCartIcon, StyledItemNumber } from "./OpenCartButton.styled";
+import { useCart } from "../../context/CartContext";
 
 export default function OpenCartButton({ handleOpenCart }) {
+  const { getItemsQuantity } = useCart();
+
   return (
     <StyledCartButton onClick={handleOpenCart}>
       <StyledCartIcon />
+      {getItemsQuantity() > 0 && <StyledItemNumber>{getItemsQuantity()}</StyledItemNumber>}
     </StyledCartButton>
   );
 }
--- src/components/Layout/OpenCartButton.styled.jsx
+++ src/components/Layout/OpenCartButton.styled.jsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import {IoMdCart} from "react-icons/io";
+import { IoMdCart } from "react-icons/io";
 
 export const StyledCartIcon = styled(IoMdCart)`
   color: black;
@@ -8,8 +8,26 @@ export const StyledCartIcon = styled(IoMdCart)`
 `;
 
 export const StyledCartButton = styled.button`
+  position: relative;
   padding: 1.3rem;
   border-radius: 50%;
   background: white;
   cursor: pointer;
+  border: none;
+`;
+
+export const StyledItemNumber = styled.span`
+  position: absolute;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  left: 0;
+  bottom: 0.1px;
+  color: #252525;
+  border-radius: 50%;
+  background: #ffb840;
+  width: 10px;
+  height: 10px;
+  padding: 0.9rem;
+  font-weight: bolder;
 `;
